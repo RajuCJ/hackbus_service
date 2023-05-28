d27f8a7c1d57d3035067d780e25066f748e44059
RajuCJ <rajureegan21@gmail.com>
2023-05-28 16:15:42 +0530

    initial commit

---
+++ src/main/java/com/prod/hackbus/HackbusApplication.java
@@ -0,0 +1,13 @@
+package com.prod.hackbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prod/hackbus/HackbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prod.hackbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
